a8aa9ad4a6fb50c4b47735ff02ca7da691ea40bb
David Rodriguez <rodriguezdavid089@gmail.com>
2019-08-07 09:22:52 -0600

    Revert "Auto stash before merge of "Developer" and "origin/Developer""
    This reverts commit 5cc481eb826a47c6cbf96176c8cb9111563c109e.

--- ControlNotas/src/main/java/com/control/entity/Bachillerato.java
+++ ControlNotas/src/main/java/com/control/entity/Bachillerato.java
@@ -45,7 +45,7 @@ public class Bachillerato implements Serializable {
     private Character seccionBachillerato;
     @JoinColumn(name = "id_especialidad", referencedColumnName = "id_especialidad")
     @ManyToOne
-    private Especialidad idEspecialidad;
+    private Especialidades idEspecialidad;
 
     public Bachillerato() {
     }
@@ -78,11 +78,11 @@ public class Bachillerato implements Serializable {
         this.seccionBachillerato = seccionBachillerato;
     }
 
-    public Especialidad getIdEspecialidad() {
+    public Especialidades getIdEspecialidad() {
         return idEspecialidad;
     }
 
-    public void setIdEspecialidad(Especialidad idEspecialidad) {
+    public void setIdEspecialidad(Especialidades idEspecialidad) {
         this.idEspecialidad = idEspecialidad;
     }
 
